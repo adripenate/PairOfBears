b55e218de4aabd467654cc865d786d4605a43e49
adriana <adrianapenate98@gmail.com>
2021-02-11 15:29:07 +0000

    Red test - test_should_not_find_pairs

---
+++ FindPairsTest.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class FindPairs:
+
+    @classmethod
+    def ofBears(cls, param):
+        pass
+
+
+class FindPairsTest(unittest.TestCase):
+
+    def test_should_not_find_pairs(self):
+        self.assertEqual("", FindPairs.ofBears("aGSTBaS18"))
+
+
+if __name__ == '__main__':
+    unittest.main()
