3af87d689177beb7b051841494a512c3f8b034eb
adriana <adrianapenate98@gmail.com>
2021-02-11 15:40:07 +0000

    Red test - test_should_find_pair_B8

--- FindPairsTest.py
+++ FindPairsTest.py
@@ -18,6 +18,9 @@ class FindPairsTest(unittest.TestCase):
     def test_should_find_pair_8B(self):
         self.assertEqual("8B", FindPairs.ofBears("8B"))
 
+    def test_should_find_pair_B8(self):
+        self.assertEqual("B8", FindPairs.ofBears("B8"))
+
 
 if __name__ == '__main__':
     unittest.main()
