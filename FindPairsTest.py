9047424b902880153f8c27dad1d18f5755691a61
adriana <adrianapenate98@gmail.com>
2021-02-11 15:38:41 +0000

    Refactor - Tests modified

--- FindPairsTest.py
+++ FindPairsTest.py
@@ -5,7 +5,7 @@ class FindPairs:
 
     @classmethod
     def ofBears(cls, param):
-        if param == "aGST8BaS18":
+        if param == "8B":
             return "8B"
         return "";
 
@@ -13,10 +13,10 @@ class FindPairs:
 class FindPairsTest(unittest.TestCase):
 
     def test_should_not_find_pairs(self):
-        self.assertEqual("", FindPairs.ofBears("aGSTBaS18"))
+        self.assertEqual("", FindPairs.ofBears("a"))
 
     def test_should_find_pair_8B(self):
-        self.assertEqual("8B", FindPairs.ofBears("aGST8BaS18"))
+        self.assertEqual("8B", FindPairs.ofBears("8B"))
 
 
 if __name__ == '__main__':
