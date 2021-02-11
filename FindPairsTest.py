1a81b4f4b7e7cf4ab7f718139e2f1e31eff0e18d
adriana <adrianapenate98@gmail.com>
2021-02-11 15:33:22 +0000

    Red test -

--- FindPairsTest.py
+++ FindPairsTest.py
@@ -13,6 +13,9 @@ class FindPairsTest(unittest.TestCase):
     def test_should_not_find_pairs(self):
         self.assertEqual("", FindPairs.ofBears("aGSTBaS18"))
 
+    def test_should_find_pair_8B(self):
+        self.assertEqual("8B", FindPairs.ofBears("aGST8BaS18"))
+
 
 if __name__ == '__main__':
     unittest.main()
